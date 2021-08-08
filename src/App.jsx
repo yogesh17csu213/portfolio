756cdcea318ecd941d8a0c723fe4f6a066b8085c
Yogesh <30828620+yogesh17csu213@users.noreply.github.com>
2021-08-08 22:56:51 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
 							<Proficiency />
 							<Education />
 							<Experience />
-							<Feedbacks />
+							{/* <Feedbacks /> */}
 							<Projects />
 							<GithubProfile />
 						</>
